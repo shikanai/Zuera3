e38812ea72ac549466e818f655df96693a9ddefa
felipear <feliperosasd@gmail.com>
2015-04-14 22:12:52 -0300

    Update LlvmBranch.java
    Parte do LlvmBranch.java

--- PROJETO2/minijava-alunos/src/llvmast/LlvmBranch.java
+++ PROJETO2/minijava-alunos/src/llvmast/LlvmBranch.java
@@ -1,14 +1,21 @@
 package llvmast;
 public  class LlvmBranch extends LlvmInstruction{
 
+    public LlvmValue cond;
+	public LlvmLabelValue brTrue;
+	public LlvmLabelValue brFalse;
+	
     public LlvmBranch(LlvmLabelValue label){
-
+    	this(null, label, null);
     }
     
     public LlvmBranch(LlvmValue cond,  LlvmLabelValue brTrue, LlvmLabelValue brFalse){
-
+    	this.cond = cond;
+    	this.brTrue = brTrue;
+    	this.brFalse = brFalse;
     }
-
+    
+    //Falta esse método
     public String toString(){
 		return null;
     }
