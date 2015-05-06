c0c79fd7bf293b1ee739aec5f6951e0b86ac911b
felipear <feliperosasd@gmail.com>
2015-05-06 00:20:14 -0300

    Limpando código 1 - Store

--- PROJETO2/minijava-alunos/src/llvmast/LlvmStore.java
+++ PROJETO2/minijava-alunos/src/llvmast/LlvmStore.java
@@ -10,13 +10,13 @@ public class LlvmStore extends LlvmInstruction{
     
     public String toString(){
 
-    	System.out.format("teste de migueh no store... detectando class: %s %s %s\n",this.address,this.address.type,this.address.type.toString());
+    	//System.out.format("teste de migueh no store... detectando class: %s %s %s\n",this.address,this.address.type,this.address.type.toString());
     	
     	if(this.address.type.toString().contains("%class.")){
-    		System.out.format("detectado class!!\n");
+    		//System.out.format("detectado class!!\n");
     		return "  store " + content.type + " " + content + ", " + address.type + "* " + address;
     	}else{
-    		System.out.format("nao detectado class...\n");
+    		//System.out.format("nao detectado class...\n");
     		
     		return "  store " + content.type + " " + content + ", " + address.type + " " + address;
     	}
